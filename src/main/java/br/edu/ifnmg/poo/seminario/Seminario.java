184949eadd0e71ac2575907d6ae1b2519326848f
Bruno Vinícius Alves Nogueira <100003070+vrunobinicius@users.noreply.github.com>
2023-12-08 20:27:45 -0300

    Chain Of Responsibility & Factory Method

--- src/main/java/br/edu/ifnmg/poo/seminario/Seminario.java
+++ src/main/java/br/edu/ifnmg/poo/seminario/Seminario.java
@@ -16,6 +16,7 @@ public class Seminario {
 
     public static void main(String[] args) {
         // CHAIN OF RESPONSIBILITY
+        
         System.out.println("CHAIN OF RESPONSIBILITY:\n\n");
         AbstractDietaHandler herbivoro = new HerbivoroHandler();
         AbstractDietaHandler carnivoro = new CarnivoroHandler();
@@ -27,14 +28,16 @@ public class Seminario {
         SerVivo serVivo1 = new SerVivo("herbivoro");
         SerVivo serVivo2 = new SerVivo("carnivoro");
         SerVivo serVivo3 = new SerVivo("onivoro");
-
+        
         herbivoro.processar(serVivo1);
         herbivoro.processar(serVivo2);
         herbivoro.processar(serVivo3);
         System.out.println("FIM DO CHAIN OF RESPONSIBILITY\n\n");
+        
         // FIM DO CHAIN OF RESPONSIBILITY
 
         // FACTORY METHOD
+        
         System.out.println("FACTORY METHOD\n\n");
         Polygon p1 = PolygonFactory.getPolygon(3);
         Polygon p2 = PolygonFactory.getPolygon(4);
@@ -44,6 +47,7 @@ public class Seminario {
         System.out.println("p2: " + p2.getNumberOfSides());
         System.out.println("p3: " + p3.getNumberOfSides());
         System.out.println("\nFIM DO FACTORY METHOD\n\n");
+        
         // FIM DO FACTORY METHOD
 
     }
